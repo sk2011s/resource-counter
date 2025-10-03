e0cf2e460fbaaa24fb2c1f22dfd3190bcd90f286
sk2011se <eatebarisajjad@gmail.com>
2025-10-03 17:23:01 +0330

    better names

--- __main__.py
+++ __main__.py
@@ -5,11 +5,11 @@ from processor import add_recipe, expand_recipe
 
 recipeFormat = compile(r"^\s*(\d+\*[A-Za-z0-9_-]+)\s*:\s*(\d+\*[A-Za-z0-9_-]+)(?:\s*,\s*\d+\*[A-Za-z0-9_-]+)*\s*$")
 
-parser = ArgumentParser(prog="recipe ai", description="solve recipes", epilog="barname khubie na?")
+parser = ArgumentParser(prog="resource counter", description="count resources", epilog="It's a good program, don't you agree? XD")
 
 parser.add_argument("-e", "--expand", type=str)
 parser.add_argument("-c", "--count", type=int, default=1)
-parser.add_argument("-r", "-recipe", type=str)
+parser.add_argument("-r", "-recipe", type=str, help="-r (<count>*<item>:<count>*<item>, <count>*<item>, ...)")
 
 argv = parser.parse_args(argv[1:])
 
@@ -17,7 +17,7 @@ if argv.r:
     if recipeFormat.fullmatch(argv.r):
         add_recipe(argv.r)
     else:
-        raise ValueError("Invalid recipe format")
+        raise ValueError("Invalid recipe format (<count>*<item>:<count>*<item>, <count>*<item>, ...)")
 
 if argv.expand:
     expanded = expand_recipe(argv.expand, argv.count)
